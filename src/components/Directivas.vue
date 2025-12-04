0a0282893b8b80f8df0541cb25da53368fb0dad0
Yaniry Florez <ymflorez@gmail.com>
2025-12-03 19:14:59 -0500

    Taller16- YFlorez- Modificadores vue

--- src/components/Directivas.vue
+++ src/components/Directivas.vue
@@ -1,31 +1,50 @@
 <template>
   <div>
+    <h2>Guardar con botón</h2>
     <label for="id_nombre">Nombre</label>
     <input v-model="nombre" id="id_nombre" type="text" />
     <p>{{ nombre }}</p>
     <label for="id_apellido">Apellido</label>
     <input v-model="apellido" id="id_apellido" type="text" />
-    <br>
+    <br />
     <br />
 
     <button v-on:click="imprimirNombre()">Imprimor nombre</button>
     <button v-on:click="agregarEstudiante()">Agregar Estudiante</button>
+    <hr />
+    <h2>Guardar con Enter</h2>
+    <label for="nombre_1">Nombre:</label>
+    <input v-model="nombre" id="id_nombre_1" type="text" />
+    <br />
+    <br />
+    <label for="apellido_1">Apellido:</label>
+    <input
+      v-model="apellido"
+      v-on:keypress.enter="agregarEstudiante1"
+      id="id_apellido_1"
+      type="text"
+    />
 
     <ul>
-        <li v-show="nombre !== null"  
-        v-for="(estu,index) in arreglo" :key="index">{{ estu.nombre }} - {{ estu.apellido }}</li>
+      <li
+        v-show="nombre !== null"
+        v-for="(estu, index) in arreglo"
+        :key="index"
+      >
+        {{ estu.nombre }} - {{ estu.apellido }}
+      </li>
     </ul>
 
     <h2>Tabla</h2>
     <table style="border: 1px solid black">
-        <tr>
-            <th>Nombre</th>
-            <th>Apellido</th>
-        </tr>
-        <tr v-for="(estu, index) in arreglo" :key="index">
-            <td>{{ estu.nombre }}</td>
-            <td>{{ estu.apellido }}</td>
-        </tr>
+      <tr>
+        <th>Nombre</th>
+        <th>Apellido</th>
+      </tr>
+      <tr v-for="(estu, index) in arreglo" :key="index">
+        <td>{{ estu.nombre }}</td>
+        <td>{{ estu.apellido }}</td>
+      </tr>
     </table>
   </div>
 </template>
@@ -44,6 +63,26 @@ export default {
       console.log(this.nombre);
       console.log(this.apellido);
     },
+
+    agregarEstudiante1(event) {
+      console.log("evento keypress");
+      if (event.charCode !== 13) return;
+
+      console.log("presiono enter");
+      console.log("Agregar estudiante 1");
+      console.log(event);
+      console.log(event.charcode);
+      const estu = {
+        nombre: this.nombre,
+        apellido: this.apellido,
+      };
+
+      console.log("Estudiante agregado:");
+      console.log(estu);
+      this.arreglo.push(estu);
+      this.limpiarFormulario();
+    },
+
     agregarEstudiante() {
       const estu = {
         nombre: this.nombre,
@@ -60,12 +99,10 @@ export default {
       this.nombre = "";
       this.apellido = "";
     },
-
   },
-
-
 };
 </script>
 
-<style>
+<style scoped >
+
 </style>
--- src/views/DirectivasView.vue
+++ src/views/DirectivasView.vue
@@ -1,20 +1,29 @@
 <template>
-  <div>
-    <Directivas/>
+  <div class="view-container">
+    <h2>Prueba</h2>
+    <br>
+    <Directivas />
   </div>
 </template>
 
 <script>
-import Directivas from '../components/Directivas.vue'
+import Directivas from "../components/Directivas.vue";
 
 export default {
-    components:{
-        Directivas,
-    },
-
-}
+  components: {
+    Directivas,
+  },
+};
 </script>
 
-<style>
-
+<style scoped>
+.view-container {
+  display: flex;
+  justify-content: center;
+  align-content: center;
+}
+h2 {
+  color: #42b983;
+  font-size: 50px;
+}
 </style>
